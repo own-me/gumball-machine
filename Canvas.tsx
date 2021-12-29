76fd9cffea3cb78188984fcd6770c17d03abbfe5
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-12-28 20:16:07 -0800

    basic gumball machine meshes and geometrys

--- Canvas.tsx
+++ Canvas.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Canvas as ThreeCanvas } from '@react-three/fiber'
-import Box from './components/Box';
+import GumballMachine from './components/GumballMachine';
 
 const Canvas = (props) => {
     return (
         <ThreeCanvas>
-            <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
+            <GumballMachine />
         </ThreeCanvas>
   )
 }
