b0a8e208ceb7d880e465d47e21947214b4a42fc9
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-12-28 21:54:50 -0800

    add floor and more styling

--- Canvas.tsx
+++ Canvas.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Canvas as ThreeCanvas } from '@react-three/fiber'
 import GumballMachine from './components/GumballMachine';
+import Floor from './components/Floor';
 
 const Canvas = (props) => {
     return (
         <ThreeCanvas>
-            <pointLight position={[10, 10, 10]} />
+            <pointLight position={[2, 10, 10]} />
             <GumballMachine />
+            <Floor />
         </ThreeCanvas>
   )
 }
--- components/GumballMachine.tsx
+++ components/GumballMachine.tsx
@@ -9,12 +9,13 @@ const GumballMachine = (props) => {
         <group ref={ref} position={[0, -1, 0]}>
             <mesh position={[0, 2, 0]}>
                 <sphereGeometry args={[1.5, 40, 20]} />
-                <meshToonMaterial color={'red'} />
+                <meshToonMaterial color={'red'} opacity={0.2} />
             </mesh>
             <mesh position={[0, 0, 0]}>
                 <cylinderGeometry args={[1, 1.5, 2, 10]} />
                 <meshToonMaterial color={"#D842FE"} />
             </mesh>
+          
         </group>
     )
 }
