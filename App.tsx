362a5403c9a37007dee6729bd73b8704f404ae46
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-12-28 22:41:20 -0800

    add 2d UI layer and demo wallet button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import styled from "styled-components";
 import Canvas from "./Canvas";
+import Ui from "./Ui";
+
+const AppContainer = styled.div`
+    position: relative;
+    height: 100%;
+    width: 100%;
+`;
 
 function App() {
     return (
-        <>
+        <AppContainer>
             <Canvas />
-        </>
+            <Ui />
+        </AppContainer>
     );
 };
 
